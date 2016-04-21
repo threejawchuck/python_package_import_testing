675ace719863c9e0e5f6843135da4a1371ffca07
Jonathan King <Jonathan.King@raytheon.com>
2016-04-21 09:43:39 -0700

    now you can execute python -m graphics

---
+++ graphics/__main__.py
@@ -0,0 +1,43 @@
+#import primitive
+
+# make sure you can talk to fill 
+from primitive import fill
+fill.hello_fill()
+fill.hello_fill_and_text()
+
+fill.hello_fill_and_jpg()
+
+###############################
+# now we want to talk directly to foo which is in contrib/mypackage
+
+# this doesn't work b/c contrib isn't a package
+# furthermore, it's brittle so don't do it
+# from contrib.mypackage.A import foo
+
+
+##############################
+# let's talk to png which talks to foo
+# png is part of package graphics.formats which is in the local dir ./
+# foo is part of package mypackage.A which is in the contrib directory ./contrib
+# but this file shouldn't have to worry about that.  png should take care of it
+
+# first make sure we can talk to png
+#from graphics.formats import png <- explicit but names the package
+# be more package name agnostic
+# from .formats import png <- either works
+from formats import png
+png.hello_png()
+
+# png isn't going to know how to call foo
+# we have three choice, 
+# 1) extend the path in graphics.formats.png
+# 2) extend the path in graphics.formats.__init__.py
+# 3) extend the path right here in river city
+
+# i think #3 is the cleanest
+import sys
+import os
+sys.path.insert(0,os.path.join ( os.path.join (os.path.dirname (__file__), "..") ,"contrib" ))
+
+# now call the combo package call
+png.hello_png_and_foo()

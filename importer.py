f0c5d0f0422c9c83829d41034dfc90d319875cf2
Jonathan King <Jonathan.King@raytheon.com>
2016-04-21 09:43:21 -0700

    updated the importer

--- importer.py
+++ importer.py
@@ -1,27 +1,30 @@
-
-# this is the same directory so it works
-import graphics
-
 # make sure you can talk to fill 
+# the long way of importing
+import graphics.primitive.fill
+graphics.primitive.fill.hello_fill()
+
+
+# that's a lot to type!  if we just want to put fill into our namespace, try this
 from graphics.primitive import fill
-fill.hello_fill()
+fill.hello_fill()  # much better!
+
+# this function calls fill and fill calls a function in text (same subpackage)
 fill.hello_fill_and_text()
 
+# this function calls fill and fill calls a function in jpg (different subpackage)
 fill.hello_fill_and_jpg()
 
-###############################
-# now we want to talk directly to foo which is in contrib/mypackage
+# we could be crass and put everything in our global space
+from graphics.primitive.fill import *
+hello_fill() # naughty script.  naughty. 
 
-# this doesn't work b/c contrib isn't a package
-# furthermore, it's brittle so don't do it
-# from contrib.mypackage.A import foo
-
+##############################
 
 ##############################
 # let's talk to png which talks to foo
 # png is part of package graphics.formats which is in the local dir ./
 # foo is part of package mypackage.A which is in the contrib directory ./contrib
-# but this file shouldn't have to worry about that.  png should take care of it
+# but who should be in charge of resolving this conundrum? png? this file?
 
 # first make sure we can talk to png
 from graphics.formats import png
@@ -33,11 +36,14 @@ png.hello_png()
 # 2) extend the path in graphics.formats.__init__.py
 # 3) extend the path right here in river city
 
-# i think #3 is the cleanest
+# i think #3 is the cleanest as it's divide and conquor
+# let the calling script (this one) understand how to modify the path to include
+# the package in question
+# and let the function, which has to use that package, understand how the package 
+# is constructed.
 import sys
 import os
 sys.path.insert(0,os.path.join (os.path.dirname (__file__), "contrib"))
-
 
 # now call the combo package call
 png.hello_png_and_foo()
